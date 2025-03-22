03227286dac200a1db70905d4f0a87418bf0c83f
wuyingxue1230 <wuyingxue1230@gmail.com>
2025-03-22 13:51:57 +0800

    生成框架

---
+++ src/renderer.js
@@ -0,0 +1,8 @@
+// 这个文件是渲染进程的主要JavaScript文件
+// 你可以在这里编写与UI交互的JavaScript代码
+console.log('renderer.js 加载成功！')
+
+// 可以添加按钮点击等事件处理函数
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM完全加载和解析')
+})
